c4077847e26d81be08471070fce8d46af98648e0
Suranga Panagamuwa Gamage <suranga.gamage@one-agency.be>
2017-10-18 23:01:23 +0200

    add initial compare command

--- src/Command/CompareCommand.php
+++ src/Command/CompareCommand.php
@@ -8,6 +8,7 @@ namespace surangapg\Haunt\Command;
 use surangapg\Haunt\Component\Discovery;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class CompareCommand extends Command {
@@ -17,15 +18,17 @@ class CompareCommand extends Command {
    */
   protected function configure() {
     $this->setName('compare')
+      ->addOption('source', NULL, InputOption::VALUE_REQUIRED, 'The directory where the source files are located.', 'artifact')
       ->setDescription('Compare all the different screenshots for the project.');
   }
 
   /**
    * @inheritdoc
    */
-  public function run(InputInterface $input, OutputInterface $output) {
+  public function execute(InputInterface $input, OutputInterface $output) {
+    $sourceDir = $input->getOption('source');
 
-    $discovery = new Discovery(getcwd(), null, null, $output);
+    $discovery = new Discovery($sourceDir, null, null, $output);
     $discovery->discover();
   }
 
